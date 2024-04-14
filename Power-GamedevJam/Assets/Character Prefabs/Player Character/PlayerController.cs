7f5f8bdc5080ea2dbf1fcd1a4033de58df6613bd
axu1672 <axu1672@gmail.com>
2024-04-13 23:07:41 -0400

    WASD Move

--- Power-GamedevJam/Assets/Character Prefabs/Player Character/PlayerController.cs
+++ Power-GamedevJam/Assets/Character Prefabs/Player Character/PlayerController.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public float bSpeed = 0.1f;
+    private Transform trn;
+    private Vector2 PlayerPosition
+    {
+        get
+        {
+            return new Vector2(trn.position.x, trn.position.y);
+        }
+        set
+        {
+            trn.position = new Vector3(value.x, value.y, trn.position.z);
+        }
+    }
+    bool move = false;
+    private Vector2 TargetLocation;
     // Start is called before the first frame update
     void Start()
     {
-        
+        trn = gameObject.transform;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bool Up = Input.GetKey(KeyCode.W);
+        bool Down = Input.GetKey(KeyCode.S);
+        bool Left = Input.GetKey(KeyCode.A);
+        bool Right = Input.GetKey(KeyCode.D);
+        int HorizontalMovement = 0;
+        int VerticalMovement = 0;
+        if ((!Up && !Down) || (Up && Down))
+        {
+            VerticalMovement = 0;
+        }
+        else
+        {
+            VerticalMovement = Up ? 1 : -1;
+        }
+
+        if ((!Left && !Right) || (Left && Right))
+        {
+            HorizontalMovement = 0;
+        }
+        else
+        {
+            HorizontalMovement = Left ? -1 : 1;
+        }
+
+        Vector2 direction = new Vector2(HorizontalMovement, VerticalMovement);
+        rb.velocity = direction;   
     }
 }
